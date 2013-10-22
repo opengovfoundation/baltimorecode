2708d800b35b7d9e8c1a39d50a23e3175520e9a0
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-10-22 17:18:41 -0400

    Fixing issue where Exceptions must be referenced in the proper namespace.

--- htdocs/admin/xml/Decoder/Section.php
+++ htdocs/admin/xml/Decoder/Section.php
@@ -6,18 +6,18 @@
 		public $patterns = array();
 		public $parents = array();
 		public $xml;
-		
+
 		public function __construct($identifier = null, $content = null, $patterns = null){
 			$this->identifier = $identifier;
 			$this->content = $content;
 			$this->patterns = $patterns;
 		}
-		
+
 		public function addParent($level, $label, $identifier, $title){
 			if(!isset($level) || !isset($label) || !isset($identifier) || !isset($title)){
-				throw new Exception("Unable to create parent!\nLevel: $level\nLabel: $label\nIdentifier: $identifier\nTitle: $title\n");
+				throw new \Exception("Unable to create parent!\nLevel: $level\nLabel: $label\nIdentifier: $identifier\nTitle: $title\n");
 			}
-			
+
 			//Create associative parent array
 			$parent = array(
 				'level'			=> trim($level),
@@ -25,26 +25,26 @@
 				'identifier'	=> trim($identifier),
 				'title'			=> trim($title)
 			);
-			
+
 			//Add parent item to the parents array
 			array_push($this->parents, $parent);
 		}
-		
+
 		public function setContent($content){
 			$this->content = $content;
 		}
-		
+
 		public function setPatterns($patterns){
 			$this->patterns = $patterns;
 		}
-		
+
 		/**
 		 * 	Create SimpleXML string from this Section object
 		 */
 		public function toXML(){
 			//Create the simple xml object
 			$law = simplexml_load_string('<?xml version="1.0" encoding="utf-8"?><law></law>');
-			
+
 			//If this section has parents, add them to the structure element
 			if(isset($this->parents) && count($this->parents) > 0){
 				$structureNode = $law->addChild("structure");
@@ -58,22 +58,22 @@
 					$unit->addAttribute('level', $parent['level']);
 				}
 			}
-			
+
 			//Add section information
 			$catch_line = $law->addChild('catch_line', $this->catch_line);
 			$section_number = $law->addChild('section_number', $this->identifier);
-			
+
 			//Split identifier and pop last element
 			$identifier_parts = explode('-', $this->identifier);
 			$order_by_value = array_pop($identifier_parts);
 			$order_by = $law->addChild('order_by', $order_by_value);
-			
+
 			//Add section content
 			$text = $law->addChild('text', $this->content);
-			
+
 			//Set this section's xml as the simpleXML law object
 			$this->xml = $law;
-			
+
 			//TODO: build children sections as well
 			// if(empty($children)){
 			// 				return "<section prefix=\"$identifier\">$content</section>";
@@ -82,7 +82,7 @@
 			// 				//Build children xml as well
 			// 			}
 		}
-		
+
 		/**
 		 * 	Save this Section's xml attribute as the given filename or its identifier
 		 */
@@ -93,23 +93,23 @@
 			$this->xml->asXML($filename);
 			exec("xmllint -format $filename -output $filename");
 		}
-		
+
 		public function parseChildren(){
-			
+
 			if(!isset($patterns)){
-				throw new Exception("Necessary patterns aren't set");
+				throw new \Exception("Necessary patterns aren't set");
 			}
-			
+
 			if(preg_match($pattern, $content)){
 				preg_match_all($pattern, $content, $matches);
 				$childrenContent = preg_split($pattern, $content, -1, PREG_SPLIT_OFFSET_CAPTURE);
 			}
-			
+
 			foreach($childrenContent as $index => $childContent){
 				$this->debug($childContent);
-				
-				
-				
+
+
+
 			}
 		}
 	}
