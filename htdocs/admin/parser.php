661718d5e7987a9ff9753bc91f3a38f186e68542
Bill Hunt <bill@krues8dr.com>
2014-01-17 10:26:56 -0500

    Major overhaul of parser for baltimore's data.  Better title and identifier handling.

--- htdocs/admin/parser.php
+++ htdocs/admin/parser.php
@@ -64,15 +64,16 @@
 			$subtitles = $subtitles[2];
 
 			//Split the remaining content into Subtitles
-			$subtitles = preg_split('@^<para>(Subtitles?\s[0-9A]+\s?[to]*\s?[0-9A]*)\s*(?:</para>)?$@m', $subtitles, -1, PREG_SPLIT_DELIM_CAPTURE);
+			$subtitles = preg_split('@^<para>(Subtitles?\s[0-9A]+\.?\s?[to]*\s?[0-9A]*)@m', $subtitles, -1, PREG_SPLIT_DELIM_CAPTURE);
 
 			//Loop through the subtitles
 			foreach($subtitles as $index => $content){
+
 				// Parts are sub-subtitles (?);
 				unset($part);
 
 				if($index % 2 != 0){//Odd indexes are the subtitle labels
-					$ret = preg_match_all('@Subtitles?\s([0-9A]+)\s?[to]*\s?([0-9A]*)@', $content, $matches);
+					$ret = preg_match_all('@Subtitles?\s([0-9A]+)\.?\s?[to]*\s?([0-9A]*)@', $content, $matches);
 
 					if($ret === 0 || $ret === false){
 						throw new Exception("Subtitle index not found.\n".
@@ -92,7 +93,20 @@
 					$subtitle_content = $content;
 
 					//Split the subtitle content on the Section identifiers
-					$sections = preg_split('@\n<para>(&#167;)+@', $subtitle_content);
+					$sections = preg_split('@\n<para>&#167;+ ([0-9A-Z]+-?[0-9A-Z]*\.?\s+)@', $subtitle_content, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+					// We have no way to keep the section identifier numbers from being
+					// split as well, so we recombine those back in here:
+					foreach($sections as $i => $section)
+					{
+						if(($i % 2) == 1)
+						{
+							$sections[$i+1] = $sections[$i] . $sections[$i+1];
+							$sections[$i] = '';
+						}
+					}
+					$sections = array_filter($sections);
+
 					if($article_index == '01'){
 						customLog($sections);
 					}
@@ -107,37 +121,38 @@
 						//If the index is zero, we have the Subtitle name
 						if($index == 0){
 							//Part 1 almost always comes right after the section title.
-							if(preg_match("/\n<para>(Part\.? +([0-9]+)\.)  /", $section, $matches))
+							if(preg_match("/<para>(Part\.? +([0-9]+)\.)(.*?)<\/para>/m", $section, $matches))
 							{
-								list($subtitle_title, $part_name) = preg_split("/\n<para>Part\.? +[0-9]+\.  /", $section);
-								$subtitle_title = trim(strip_tags($subtitle_title));
-								$part_name = trim(strip_tags($part_name));
+								$pieces = preg_split("/(<para>Part\.? +[0-9]+\. )/", $section, 2, PREG_SPLIT_DELIM_CAPTURE);
 
 
-								$parts = preg_split("/\n+/", $part_name, 2);
-								if(count($parts) == 2)
-								{
-									list($part_name, $section) = $parts;
-								}
-								elseif(count($parts) == 1)
-								{
-									$parts_name = $parts[0];
-								}
-								unset($parts);
+
+								$subtitle_title = array_shift($pieces);
+								$subtitle_title = str_replace("\n", " ",
+									trim(strip_tags($subtitle_title)));
+
+								$section = join($pieces);
+
+								$section = trim(preg_replace(
+									"/(<para>(Part\.? +([0-9]+)\.)(.*?)<\/para>)/m", '',
+									$section));
+
+								$part_name = trim(strip_tags($matches[3]));
 
 								$part = array(
 									'index' => $matches[2],
 									'title' => $part_name
 								);
 
-								if(!$section)
+								if(!strlen($section))
 								{
 									continue;
 								}
 
 							}
 							else{
-								$subtitle_title = strip_tags($section);
+								//if($article_index == '22') var_dump('Section', $article_index, substr($section, 0, 40));
+								$subtitle_title = str_replace("\n", " ", trim(strip_tags($section)));
 								continue;
 							}
 
@@ -150,28 +165,33 @@
 						// the *top* or *bottom* of sections.  That's a pretty safe bet,
 						// but keep an eye on this!
 
-						if(preg_match("/\n\n<para>Part\.? +([0-9]+)\. +/", $section, $matches))
+						if(preg_match("/<para>Part\.? +([0-9]+)\. +/", $section, $matches))
 						{
-							$pieces = preg_split("/\n\n<para>(Part\.? +[0-9]+\. +.*)(?:\n|$)/", $section, -1, PREG_SPLIT_DELIM_CAPTURE);
+							$pieces = preg_split("/(<para>Part\.? +[0-9]+\. +.*?<\/para>)/m", $section, -1, PREG_SPLIT_DELIM_CAPTURE);
 							$pieces = array_filter($pieces);
 							$section = '';
 
-							foreach($pieces as $key => $piece)
+							foreach($pieces as $piece)
 							{
 								$piece = trim($piece);
-								if(preg_match('/<para>Part\.? +([0-9]+)\. +(.*)/', $piece, $matches))
+								if(preg_match('/<para>Part\.? +([0-9]+)\. +(.*)<\/para>/m', $piece, $matches))
 								{
+
 									$temp_part = array(
 										'index' => $matches[1],
 										'title' => strip_tags($matches[2])
 									);
 								}
-								else
-								{
-									$section .= strip_tags($piece);
-								}
 							}
 
+							$section = trim(preg_replace(
+								"/(<para>(Part\.? +([0-9]+)\.)(.*?)<\/para>)/m", '',
+								$section));
+
+							if(!strlen($section))
+							{
+								continue;
+							}
 						}
 
 						//Split the first line (Section top-level information)
@@ -208,31 +228,29 @@
 						if($article_index == '22')
 						{
 							//grab the section catch title
-							$catch_line = preg_replace('@\d+( (to|-) \d+)?\.?@', '', $lines[0]);
+							$catch_line = preg_replace('@[0-9A]+( (to|-) [0-9A]+)?\.?@', '', $lines[0]);
 
 							//grab the section number
-							$ret = preg_match('@((\d+)( (?:to|-) (\d+))?)\.?@', $lines[0], $identifier);
+							$ret = preg_match('@(([0-9A]+)( (?:to|-) ([0-9A]+))?)\.?@', $lines[0], $identifier);
 
 							$order_by = $identifier[2];
 						}
 						else
 						{
 							//grab the section catch title
-							$catch_line = preg_replace('@(\d+[A-Z]?-\d+\s?(to)\s?\d*[A-Z]?-?\d*)\.?@', '', $lines[0]);
+							$catch_line = preg_replace('@(\d+[A-Z]?(\s?-\s?\d+(\s?(to)\s?\d*[A-Z]?-?\d*)?)?)\.?@', '', $lines[0]);
 
 							//grab the section number
-							$ret = preg_match('@(\d+[A-Z]?-?\d*\s?(to)*\s?\d*[A-Z]?-?\d*)\.?@', $lines[0], $identifier);
+							$ret = preg_match('@(\d+[A-Z]?-?(\d+)\s?(to)*\s?\d*[A-Z]?-?\d*)\.?@', $lines[0], $identifier);
 
-							$order_by = $identifier[1];
+							$order_by = $identifier[2];
 						}
 
 						if($ret === 0 || $ret === false){
 							throw new Exception("Error getting section number in $filename " .
-								"for line: " . print_r($lines[1], true) . "\n" .
+								"for line: " . print_r($lines[0], true) . "\n" .
 								"****\n" .
 								"Subtitle: " .
-								substr($sections[$index-2], 0, 78) ."\n\n".
-								substr($sections[$index-1], 0, 78) ."\n\n".
 								substr($section, 0, 78));
 						}
 
